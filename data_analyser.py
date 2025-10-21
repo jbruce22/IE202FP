363781e681a42bd8a9d7ea88b756a8835c5640b6
jbruce22 <jbruce22@vols.utk.edu>
2025-10-21 14:25:57 -0400

    Update data_analyser.py

--- data_analyser.py
+++ data_analyser.py
@@ -1,4 +1,23 @@
-import panda as pd
-import numpy as np
 import os
+import pandas as pd
+import numpy as np
 
+class DataAnalyser:
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+    
+    def summarize(self):
+        pass
+
+    def tables(self):
+        pass
+
+class Control:
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+    
+    def summarize(self):
+        pass
+
+    def tables(self):
+        pass
