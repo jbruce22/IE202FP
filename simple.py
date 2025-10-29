3aa03cbff628df1f579261dfacf6c5ee2ee259b0
jbruce22 <jbruce22@vols.utk.edu>
2025-10-29 12:00:56 -0400

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import os
+
+class Analyzer:
+    def __init__(self):
+        self.files = []
+        self.elev_stair = ''
+        self.start_floor = 0
+        self.end_floor = 0
+        self.data = []
+
+
+    def setup(self):
+        con_file = "Control_Data.xlsx"
+        reg_file = "Data.xlsx"
+        os_path = os.path
+        while True:
+            user_input = input("B: Both, C: Control, R: Regular ").upper()
+            if user_input == "B":
+                self.files = [os_path.join('data', con_file), os_path.join('data', reg_file)]
+                break
+            elif user_input == "C":
+                self.files = [os_path.join('data', con_file)]
+                break
+            elif user_input == "R":
+                self.files = [os_path.join('data', reg_file)]
+                break
+            else:
+                print('Invalid Input')
+        while True:
+            user_input = input("E: Elevator, S: Stairs ").upper()
+            if user_input == "E":
+                self.elev_stair = 'elev'
+                break
+            elif user_input == "S":
+                self.elev_stair = 'stair'
+                break
+            else:
+                print('Invalid Input')
+
+        while True:
+            user_input = int(input("Enter start floor: "))
+            if user_input >= 0 and user_input <= 5:
+                self.start_floor = user_input
+                break
+            else:
+                print('Invalid Input')
+
+        while True:
+            user_input = int(input("Enter end floor: "))
+            if user_input >= 0 and user_input <= 5 and user_input != self.start_floor:
+                self.end_floor = user_input
+                break
+            else:
+                print('Invalid Input')
+
+    def get_data(self, elev_stair, start_floor, end_floor):
+        for file in self.files:
+            plc = 'x'
